b0b8b2da61d7a8cd69efc1af97319a724f6b38c0
nikhildubey-23 <ravannick0089@gmail.com>
2026-04-07 20:27:41 +0530

    push tome

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/approvals')
+def approvals():
+    return render_template('approvals.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
